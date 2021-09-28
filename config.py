b61b90d25090fa3072383ddcf3087fa6adc1b17c
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-28 10:18:12 +0300

    move database uri to development

--- config.py
+++ config.py
@@ -1,9 +1,18 @@
 import os
+
+from flask_sqlalchemy import SQLAlchemy
 class Config:
     '''
     General configuration parent class
     '''
-    pass
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USER_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     '''
@@ -21,7 +30,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings    
     '''
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://royrasugu:qwerty@localhost/blog'
     DEBUG = True
 
 config_options = {
