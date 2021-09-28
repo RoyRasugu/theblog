bfb6244bcfaefae2d8692fde2ba1743da61e9b8a
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-09-28 04:33:29 +0300

    create forms for CRUD functionality

---
+++ app/main/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField,TextAreaField,StringField
+from wtforms import validators
+from wtforms.validators import Required
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('update your bio for a better feel',validators = [Required()])
+    submit = SubmitField('Submit')
+
+class AddBlog(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    content = TextAreaField('Content', validators=[Required()])
+    submit = SubmitField('Post')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Add comment',validators = [Required()])
+    submit = SubmitField('Submit')
